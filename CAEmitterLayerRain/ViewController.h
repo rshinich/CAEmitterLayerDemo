01961d99553af55eade61e8868fe0af5cc9187a2
zhangzr <zhangzr@vastio.com>
2017-08-07 10:29:11 +0800

    Initial Commit

---
+++ CAEmitterLayerRain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAEmitterLayerRain
+//
+//  Created by 张忠瑞 on 2017/8/7.
+//  Copyright © 2017年 张忠瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CAEmitterLayerRain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CAEmitterLayerRain
+//
+//  Created by 张忠瑞 on 2017/8/7.
+//  Copyright © 2017年 张忠瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
