d4021c85a02290359d080220ee5d716d1672b4cd
zhangzr <zhangzr@vastio.com>
2017-08-16 17:30:05 +0800

    CAEmitterLayer
    xxx

--- CAEmitterLayerRain/ViewController.m
+++ CAEmitterLayerRain/ViewController.m
@@ -10,19 +10,61 @@
 
 @interface ViewController ()
 
+@property (nonatomic ,strong) CAEmitterCell     *waterCell;
+@property (nonatomic ,strong) CAEmitterLayer    *waterLayer;
+
+@property (nonatomic ,strong) UIView            *rainView;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setUpEmitterLayer];
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)setUpEmitterLayer
+{
+    self.rainView = [[UIView alloc] init];
+    self.rainView.frame = self.view.bounds;
+    self.rainView.backgroundColor = [UIColor clearColor];
+
+    
+    self.waterCell = [CAEmitterCell emitterCell];
+    self.waterCell.birthRate = 5.0;
+    self.waterCell.lifetime = 40.0;
+    self.waterCell.lifetimeRange = 10;
+    self.waterCell.velocity = 30;
+    self.waterCell.velocityRange = 10;
+    self.waterCell.yAcceleration = 30;
+    self.waterCell.emissionRange = 0;
+    self.waterCell.scale = 0.1;
+    self.waterCell.scaleRange = 0.05;
+    self.waterCell.contents = (id)[[UIImage imageNamed:@"Ovalwater"] CGImage];
+    
+    
+    self.waterLayer = [CAEmitterLayer layer];
+    self.waterLayer.emitterPosition = CGPointMake(self.view.bounds.size.width/ 2.0, -10);
+    self.waterLayer.emitterSize = CGSizeMake(self.view.bounds.size.width, 0.0);
+    self.waterLayer.emitterShape = kCAEmitterLayerLine;
+    self.waterLayer.emitterMode = kCAEmitterLayerVolume;
+    self.waterLayer.velocity = 5;
+    self.waterLayer.emitterCells = @[self.waterCell];
+
+    [self.rainView.layer addSublayer:self.waterLayer];
+    [self.view addSubview:self.rainView];
+}
+
+
+
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
